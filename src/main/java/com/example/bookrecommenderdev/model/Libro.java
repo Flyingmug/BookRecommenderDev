373c04c7a37372fd11ae3ba97edc47d904351e24
Flyingmug <sebian.selimi@gmail.com>
2025-08-03 16:11:19 +0200

    Modified testing class "Libro"

--- src/main/java/com/example/bookrecommenderdev/model/Libro.java
+++ src/main/java/com/example/bookrecommenderdev/model/Libro.java
@@ -2,4 +2,11 @@ package com.example.bookrecommenderdev.model;
 
 public class Libro {
 
+  String idLibro;
+  int annoPubblicazione;
+  String titolo;
+  String autori;
+  String editore;
+  String categorie;
+
 }

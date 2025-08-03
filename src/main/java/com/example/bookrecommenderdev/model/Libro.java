93cb4fc469f41a78e31d9b8261a62403bcf8aa91
Flyingmug <sebian.selimi@gmail.com>
2025-08-03 16:43:34 +0200

    Modifications to "Libro" class

--- src/main/java/com/example/bookrecommenderdev/model/Libro.java
+++ src/main/java/com/example/bookrecommenderdev/model/Libro.java
@@ -1,5 +1,12 @@
 package com.example.bookrecommenderdev.model;
 
+/**
+ * Questa classe definisce le caratteristiche dei libri, composti da titolo, autore, editore, categoria,
+ * e fornisce i metodi necessari per:
+ * - settare alcuni parametri (setEditore / setCategorie)
+ * - o reperirli (getTitolo / toShortHandFullString)
+ * @author Selimi Sebian
+ * @author Moscatelli Alexander*/
 public class Libro {
 
   String idLibro;
@@ -8,5 +15,20 @@ public class Libro {
   String autori;
   String editore;
   String categorie;
+
+  /**
+   * Costruttore Libro
+   * @param autori String
+   * @param titolo String
+   * @param annoPubblicazione int
+   */
+  public Libro(String idLibro, String titolo, String autori, int annoPubblicazione, String editore, String categorie) {
+    this.idLibro = idLibro;
+    this.titolo = titolo;
+    this.autori = autori;
+    this.annoPubblicazione = annoPubblicazione;
+    this.editore = editore;
+    this.categorie = categorie;
+  }
 
 }

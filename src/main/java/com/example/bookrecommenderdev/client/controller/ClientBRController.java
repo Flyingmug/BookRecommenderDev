88bbcfd08835443b9391f2e985e55e78e7773dfc
Flyingmug <sebian.selimi@gmail.com>
2025-09-02 01:34:26 +0200

    Working resultpage/homepage switching and data querying

--- src/main/java/com/example/bookrecommenderdev/client/controller/ClientBRController.java
+++ src/main/java/com/example/bookrecommenderdev/client/controller/ClientBRController.java
@@ -3,12 +3,15 @@ package com.example.bookrecommenderdev.client.controller;
 import com.example.bookrecommenderdev.model.Libro;
 import com.example.bookrecommenderdev.server.ServerInterface;
 import javafx.fxml.FXML;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.*;
 import javafx.scene.image.Image;
+import javafx.scene.text.Font;
 import org.kordamp.ikonli.javafx.FontIcon;
 
 import java.net.URL;
@@ -48,15 +51,30 @@ public class ClientBRController {
     private VBox booksResultWrapper;
 
     @FXML
-    private FontIcon noBooksIcon;
+    private HBox noResultsTitleWrapper;
 
     private ServerInterface bookRecommender;
+
+    int i = 0;  // DEBUG
 
 
     @FXML
     public void initialize() {
         initRegistry();
+        initSetupLayout();
 
+        //
+        //
+        // TEST
+        testBooksearchpage();
+    }
+
+    /**
+     * Inizializza gli elementi grafici privati visualizzabili solo dopo certe azioni.
+     */
+    private void initSetupLayout() {
+
+        // background
         URL imageUrl = getClass().getResource("/com/example/bookrecommenderdev/assets/library-background2.jpg");
         if (imageUrl != null) {
             Image image = new Image(imageUrl.toExternalForm());
@@ -72,13 +90,22 @@ public class ClientBRController {
             centerStackContainer.setBackground(new Background(backgroundImage));
         }
 
-        // icone non visibili
-        noBooksIcon = new FontIcon("mdi2b-book-alert");
+        // icona di ricerca
+        FontIcon noBooksIcon = new FontIcon("mdi2b-book-alert-outline");
         noBooksIcon.setIconSize(38);
+        // titolo ricerca fallita
+        Label noResultsTitle = new Label("Nessun risultato trovato");
+        noResultsTitle.setFont(new Font("Arial", 30));
+        noResultsTitle.setPadding(new Insets(5, 10, 5, 10));
+        noResultsTitleWrapper = new HBox(noResultsTitle, noBooksIcon);
+        noResultsTitleWrapper.setAlignment(Pos.CENTER);
 
-        testBooksearchpage();
+        HBox.setMargin(noResultsTitleWrapper, new Insets(100, 0, 0, 0));
+
     }
-
+    /**
+     * Inizializza l'oggetto remoto del server dal repository.
+     */
     private void initRegistry() {
         try {
             Registry reg = LocateRegistry.getRegistry("localhost", 1099);
@@ -92,24 +119,27 @@ public class ClientBRController {
         }
     }
 
+    /**
+     * Caricamento pagina iniziale
+     */
     @FXML
     protected void onHomepage() {
         resultPage.setVisible(false);
-        homePage.setVisible(true);
-        resetSearchpage();
         resetHomepage();
+        homePage.setVisible(true);
     }
+    /**
+     * Ripristina gli elementi della pagina iniziale nelle loro posizioni originali
+     */
     private void resetHomepage() {
 
         if (!homePage.getChildren().contains(searchbarWrapper)) {
+            System.out.println("searchbar missing -> moving it");
             searchbarWrapper.getStyleClass().remove("searchbar-navbar");
             searchbarWrapper.getStyleClass().add("searchbar-center");
             searchbar.setText("");
             homePage.getChildren().addAll(welcomeText, searchbarWrapper, mainPageSpacer);
         }
-    }
-    private void resetSearchpage() {
-        resultTitleWrapper.getChildren().remove(noBooksIcon);
     }
 
 
@@ -120,6 +150,7 @@ public class ClientBRController {
         String input = searchbar.getText();
         System.out.println("Searched: " + input);  // DEBUG
 
+        // if input is empty do nothing
         if (input == null || input.isEmpty()) return;
 
         topSearchbar();
@@ -128,20 +159,25 @@ public class ClientBRController {
 
         try {
             List<Libro> res = bookRecommender.searchTitolo(input);
-            res.clear();
+            if ((i%2)==0)   // testing purposes
+                res.clear();
+            i++;
 
-            // log di debug e set dei risultati
+
             if (!res.isEmpty()) {
-                resultTitle.setText("Risultati");
-                System.out.println("Numero di risultati: " + res.size());
+                // Data present
+                System.out.println("Numero di risultati: " + res.size());   // DEBUG
+
+                setResultsFoundTitle(true);
                 booksResultWrapper.setVisible(true);
 
-                loadResults(res);
+                loadResults(res); // crea gli oggetti per rappresentare i dati
             } else {
+                // No results
+                System.out.println("Empty result set.");   // DEBUG
+
+                setResultsFoundTitle(false);
                 booksResultWrapper.setVisible(false);
-                resultTitle.setText("Nessun risultato");
-                resultTitleWrapper.getChildren().addLast(noBooksIcon);
-                System.out.println("Empty result set.");
             }
 
 
@@ -150,28 +186,47 @@ public class ClientBRController {
             e.printStackTrace();
         }
 
-        openResultsPage();
-
     }
     private void topSearchbar() {
-        homePage.getChildren().clear();
-        searchbarWrapper.getStyleClass().remove("searchbar-center");
-        searchbarWrapper.getStyleClass().add("searchbar-navbar");
-        navbar.getChildren().addFirst(searchbarWrapper);
+        if (!navbar.getChildren().contains(searchbarWrapper)) {
+            homePage.getChildren().clear();
+            searchbarWrapper.getStyleClass().remove("searchbar-center");
+            searchbarWrapper.getStyleClass().add("searchbar-navbar");
+            navbar.getChildren().addFirst(searchbarWrapper);
+        }
     }
-    private void openResultsPage() {
 
+    /**
+     * Rimpiazza i children del contenitore del titolo a seconda del risultato della ricerca
+     * @param success risultati trovati o meno
+     */
+    private void setResultsFoundTitle(boolean success) {
+
+        resultTitleWrapper.getChildren().clear();
+
+        if (success) {
+            resultTitleWrapper.getChildren().add(resultTitle);
+        } else {
+            resultTitleWrapper.getChildren().addAll(noResultsTitleWrapper);
+        }
     }
-
-
 
     private void loadResults(List<Libro> results) {
+
+        // elimina eventuali elementi precedenti
+        if(!booksResultDisplay.getChildren().isEmpty())
+            booksResultDisplay.getChildren().clear();
 
         for (Libro l: results) {
             VBox row = new VBox(5); // spacing inside row
             Label titolo = new Label(l.getTitolo());
             Label autore = new Label(l.getAutori());
-            Label anno = new Label(String.valueOf(l.getAnnoPubblicazione()));
+            Label anno = new Label(l.getAnnoPubblicazione() > 0 ? Integer.toString(l.getAnnoPubblicazione()) : "");
+
+//            private String formatAnno(int anno) {
+//                return anno > 0 ? Integer.toString(anno) : "";
+//            }
+//            Label anno = new Label(formatAnno(l.getAnnoPubblicazione()));
 
             row.getChildren().addAll(titolo, autore, anno);
             row.setStyle("-fx-padding: 10; -fx-border-color: #99b1e9; -fx-border-width: 0 0 1 0;");
